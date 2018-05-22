d31b7da14b6ef742a796ae34527a5da11013da2a
Bryant Torres <brt9029@gmail.com>
2018-05-22 21:19:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,10 +17,6 @@ function whileLoop(n){
 }
 
 function doWhileLoop(array){
-  function maybeTrue(){
-    return array
-  }
-  
   do {
     array.shift()
   } while (array.length > 0 && maybeTrue())
